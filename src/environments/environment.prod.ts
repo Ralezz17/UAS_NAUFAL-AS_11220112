a51c8221cbbfd907d55c719944fbd74bbe14549b
Naufal Akhdan Saputra <nakhdans@gmail.com>
2024-07-27 22:10:29 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiAnak:
+    'https://f7c13ecc-181b-452e-bc0d-9138b02d0e8a-00-2d93wugo650q0.kirk.replit.dev/',
+  apiOrtu:
+    'https://9ec07c73-cc28-49b5-b2ea-49cd3f12f088-00-3dfdwlfrv9l3y.picard.replit.dev/',
 };
